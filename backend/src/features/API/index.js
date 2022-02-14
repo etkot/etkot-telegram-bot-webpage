735705df07187c7faf963585c49c2c9d13b44846
Niilo Jaakkol <niilo.jaakkola@icloud.com>
2022-02-14 12:45:17 +0200

    Removed accidental .js files

--- backend/src/features/API/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ApiRouter = void 0;
-var express_1 = require("express");
-var Login_1 = require("./Login");
-exports.ApiRouter = (0, express_1.Router)();
-exports.ApiRouter.use('/api', Login_1.LoginRouter);
--- backend/src/features/Quote/quote.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.QuoteDBModel = void 0;
-var mongoose = require("mongoose");
-var quoteDBSchema = new mongoose.Schema({
-    quote: String,
-    name: String
-});
-exports.QuoteDBModel = mongoose.model('Quote', quoteDBSchema);
